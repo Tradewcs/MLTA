28b6eee02b6f1f1d0e29f6368c437be540d72c0b
Tradewcs <tradewcs@gmail.com>
2022-04-23 00:58:31 +0300

    Add index.html main.css script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+var input_f = document.querySelector('.input_f');
+
+var button = document.querySelector('.calculate_button');
+
+var body = document.querySelector('body');
+
+
+
+button.addEventListener('click', function() {
+	var n = (document.querySelector('.input_n')).value;
+	
+	for (let i = 0; i < Math.pow(2, n); i++) {
+		
+	}
+
+	var js = document.createElement("script");
+	js.type = 'text/javascript';
+	js.src = 'https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML';
+	document.body.appendChild(js);
+})
